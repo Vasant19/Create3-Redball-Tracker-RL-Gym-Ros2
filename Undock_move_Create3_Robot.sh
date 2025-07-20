fa716f8fe84255eb17fb453e1207e94a283595d5
vasant <vasantdave2002@gmail.com>
2025-07-20 16:50:08 -0400

    Commented out commands in bash script

--- Undock_move_Create3_Robot.sh
+++ Undock_move_Create3_Robot.sh
@@ -3,17 +3,17 @@
 ros2 action send_goal /undock irobot_create_msgs/action/Undock "{}"
 sleep 5
 
-# Turn
-for i in {1..3}
-do
-  ros2 topic pub --once /cmd_vel geometry_msgs/msg/Twist \
-    "{linear: {x: 0.2}, angular: {z: 0.3}}"
-  sleep 0.05
-done
+# # Turn
+# for i in {1..3}
+# do
+#   ros2 topic pub --once /cmd_vel geometry_msgs/msg/Twist \
+#     "{linear: {x: 0.2}, angular: {z: 0.3}}"
+#   sleep 0.05
+# done
 
-# Drive straight
-for i in {1..40}
-do
-  ros2 topic pub --once /cmd_vel geometry_msgs/msg/Twist \
-    "{linear: {x: 1.5}, angular: {z: 0.0}}"
-done
+# # Drive straight
+# for i in {1..40}
+# do
+#   ros2 topic pub --once /cmd_vel geometry_msgs/msg/Twist \
+#     "{linear: {x: 1.5}, angular: {z: 0.0}}"
+# done
